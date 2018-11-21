18da5e0d108105e4fc587dd0298d060dd3299437
Lester Reandino <lesterreandino@gmail.com>
2018-11-21 21:01:24 +0800

    can write to CSV file

--- stock_price.py
+++ stock_price.py
@@ -2,6 +2,7 @@ import requests
 import bs4
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import csv
 
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 cred = ServiceAccountCredentials.from_json_keyfile_name('../client_secret.json',scope)
@@ -13,13 +14,19 @@ req = requests.get('https://www.pesobility.com/stock')
 soup = bs4.BeautifulSoup(req.text,'lxml')
 code = soup.select("tr")
 
-
+# putting the stock data into a dictionary
 data = []
 for x in code:
 	td = x.find_all('td')
-	#print("%s - %s - %s" % (td[0].text,td[1].text,td[2].text))
 	data.append([td[0].text,td[1].text,td[2].text,td[3].text,td[4].text,td[5].text,td[6].text,td[7].text])
 
-for i in range(len(data)):
-	l = i + 1
-	sheet3.insert_row(data[i],l)
+# opening the csv
+with open('stocks.csv','w') as writeFile:
+	writer = csv.writer(writeFile)
+	writer.writerows(data)
+
+writeFile.close()
+# writing in google sheets
+# for i in range(len(data)):
+# 	l = i + 1
+# 	#sheet3.insert_row(data[i],l)

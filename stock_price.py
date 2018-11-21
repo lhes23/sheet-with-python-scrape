faece3a79b6687252cf2803605ce6e1e5a584f09
Lester Reandino <lesterreandino@gmail.com>
2018-11-21 17:08:47 +0800

    stock_price.py able to write to google sheets

--- stock_price.py
+++ stock_price.py
@@ -4,15 +4,22 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-cred = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json',scope)
+cred = ServiceAccountCredentials.from_json_keyfile_name('../client_secret.json',scope)
 client = gspread.authorize(cred)
 
 sheet3 = client.open('test scrape').worksheet('Sheet3')
 
 req = requests.get('https://www.pesobility.com/stock')
 soup = bs4.BeautifulSoup(req.text,'lxml')
-code = soup.select("td > a")
+code = soup.select("tr")
 
-for i in code:
-	for x in range(len(code)):
-		sheet3.update_cell(x,1,i.text)
+
+data = []
+for x in code:
+	td = x.find_all('td')
+	#print("%s - %s - %s" % (td[0].text,td[1].text,td[2].text))
+	data.append([td[0].text,td[1].text,td[2].text,td[3].text,td[4].text,td[5].text,td[6].text,td[7].text])
+
+for i in range(len(data)):
+	l = i + 1
+	sheet3.insert_row(data[i],l)
